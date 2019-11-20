57c035770af1f10d0464d625ab070098c16703bf
seleniadelgado <angiedelgado08@gmail.com>
2019-11-19 18:14:01 -0800

    javascript prompt practice

---
+++ javascript/stalkerjs.js
@@ -0,0 +1,8 @@
+var userName = prompt("what is your first name?")
+var lastName = prompt("what is your last name?")
+var age = prompt("what is your age?")
+var fullName = userName + " " + lastName
+
+console.log("Your name is " + fullName + ".")
+console.log("You are " + age + " years old.")
+
