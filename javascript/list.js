afb842abcc57e5ef15ee93fc7bef6867a1018b83
seleniadelgado <angiedelgado08@gmail.com>
2020-01-10 16:25:34 -0600

    todo list app

--- javascript/list.js
+++ javascript/list.js
@@ -5,15 +5,38 @@ var input = prompt("What would you like to do")
 while(input !== "quit"){
 	//handle input
 	if (input === "list") {
-		console.log(todos)
-	} else if(input === "new"){
-	//ask for new todo
+		listTodos();
+	} else if(input === "new") {
+		addTodo();
+	} else if (input === "delete"){
+		deleteTodo();
+	//ask again for new input
+	}
+	input = prompt("What would you like to do");
+}
+console.log("Okay, u quit the app");
+
+function listTodos(){
+	//list all todos
+	console.log("********")
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("********")
+}
+
+function addTodo(){
 	var newTodo = prompt("Enter new todo");
 	//add to todo's array
 	todos.push(newTodo);
+	console.log("Added todo");
+}
+function deleteTodo(){
+	//ask for index of todo to be deleted.
+	var index = prompt("Enter index of todo to delete");
+	//delete that todo.
+	//splice() cut and remove
+	todos.splice(index,1);	
+	console.log("Deleted todo");
 	}
-	//ask again for new input
-	input = prompt("What would you like to do");
-}
-}
-console.log("Okay, u quit the app");
+
