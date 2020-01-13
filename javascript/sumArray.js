43a50ada6c53d35a7205488fbbc1754a3127f361
seleniadelgado <angiedelgado08@gmail.com>
2020-01-13 14:57:14 -0600

    change to sumArray

--- javascript/sumArray.js
+++ javascript/sumArray.js
@@ -1,9 +1,11 @@
 console.log("sumArray")
+// adds every element in array.
 
 function sumArray(arr){
 	var sum = 0;
 
-	for(var i = 0; i < arr.length; i++){
+	for(var i = 0; i <
+	 arr.length; i++){
 		sum += arr[i] 
 	}
 	console.log(sum);
